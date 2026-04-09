743b26a27d0f68b759715b8ad8623eb92c37c7cf
marellano16 <arellano.m@ufl.edu>
2026-04-08 22:41:38 -0400

    remove NewProfileButton class from Buttons.h
    removed the NewProfileButton class and its comments from Buttons.h. willl insert appropriately into UserSelectButtons.h

--- Lingma/src/Buttons.h
+++ Lingma/src/Buttons.h
@@ -20,19 +20,3 @@ protected:
 };
 
 // To create a button, make a child class with its own functionality when you click on it
-
-/*==== Derived Buttons ====*/
-// New Profile Button //
-// - Used to create a new profile
-class NewProfileButton : public Button
-{
-public:
-	NewProfileButton(const sf::Vector2f& size, const sf::Vector2f& position, const sf::Color& color) : Button(size, position, color) {}
-
-	NewProfileButton(const sf::Vector2f& size, const sf::Vector2f& position, const sf::Color& color, const sf::Text& text) : Button(size, position, color, text) {}
-
-	void MousePress() override
-	{
-		// TODO: Implement functionality to create a new profile
-	}
-};
