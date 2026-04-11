05ca03ca97402f9b29362cdee92537c1768dd27c
mathews526 <santanamathew@ufl.edu>
2026-04-11 01:08:32 -0500

    DrawButton function in Button class is now virtual
    It was made virtual so that child classes can have unique draw functionality. Also, the constructor that was with text was removed because text won't be needed on buttons anymore.

--- Lingma/src/Buttons.h
+++ Lingma/src/Buttons.h
@@ -6,13 +6,12 @@ struct Button
 {
 	/*==== Constructor / Destructor ====*/
 	Button(const sf::Vector2f& size, const sf::Vector2f& position, const sf::Color& color); // Button without text
-	Button(const sf::Vector2f& size, const sf::Vector2f& position, const sf::Color& color, const sf::Text& text); // Button with text TODO: Decide on a font and download a font file
 	virtual ~Button();
 
 	/*==== Behaviors ====*/
 	virtual void MousePress() = 0; // Implement this for any child classes
 	bool Contains(sf::Vector2f mousePos) const; // Indicates whether click was within the button
-	void DrawButton(sf::RenderWindow& window);
+	virtual void DrawButton(sf::RenderWindow& window);
 	virtual void UpdatePosition(const sf::Vector2f& winSize) = 0;
 
 protected:
